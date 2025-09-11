5caf0825565c6b0b0b96f63e6a117d98b3c4a328
Chu Ngoc Son <cns1459@gmail.com>
2025-09-11 13:22:42 +0000

    Try fixing this error: `Runtime Error: Error: useAuth must be used withi

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Logo } from "@/components/icons";
+import { AuthProvider } from "@/context/auth-provider";
 
 export default function AuthLayout({
   children,
@@ -7,16 +8,18 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center bg-background px-4">
-      <div className="w-full max-w-md">
-        <div className="mb-6 flex justify-center">
-            <Link href="/dashboard" className="flex items-center gap-2 font-headline font-semibold text-2xl">
-              <Logo className="h-8 w-8 text-primary" />
-              <span>Ezi Languages</span>
-            </Link>
+    <AuthProvider>
+      <div className="flex min-h-screen w-full items-center justify-center bg-background px-4">
+        <div className="w-full max-w-md">
+          <div className="mb-6 flex justify-center">
+              <Link href="/dashboard" className="flex items-center gap-2 font-headline font-semibold text-2xl">
+                <Logo className="h-8 w-8 text-primary" />
+                <span>Ezi Languages</span>
+              </Link>
+          </div>
+          {children}
         </div>
-        {children}
       </div>
-    </div>
+    </AuthProvider>
   );
 }
