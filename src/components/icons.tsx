e459c638ca2421e1fc2212886335ad069a36543a
Chu Ngọc Sơn <chungocson12a6@gmail.com>
2025-09-15 15:34:40 +0000

    đổi lại, tui thấy bị lỗi

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
 
 interface LogoProps {
     className?: string;
@@ -8,11 +7,23 @@ interface LogoProps {
 }
 
 export const Logo = ({ className, width = 32, height = 32 }: LogoProps) => (
-    <Image
-        src="/logo.png"
-        alt="Ezi Languages Logo"
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
         width={width}
         height={height}
         className={className}
-    />
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+    >
+        <path d="m5 8 6 6" />
+        <path d="m4 14 6-6 2-3" />
+        <path d="M2 5h12" />
+        <path d="M7 2h1" />
+        <path d="m22 22-5-10-5 10" />
+        <path d="M14 18h6" />
+    </svg>
 );
