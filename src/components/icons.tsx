4ff6c7fd9ee84b2579661cf05300cf17d4e09133
Chu Ngọc Sơn <chungocson12a6@gmail.com>
2025-09-15 16:12:12 +0000

    Try fixing this error: `Build Error: Export Logo doesn't exist in target

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,19 +1,14 @@
-import React from 'react';
-
-interface LogoProps {
-    className?: string;
-    width?: number;
-    height?: number;
-}
-
-export const Logo = ({ className, width = 32, height = 32 }: LogoProps) => (
+export function Logo({ width = 32, height = 32, ...props }) {
+  return (
     <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={width}
-        height={height}
-        className={className}
-        viewBox="0 0 100 100" // Bạn có thể cần điều chỉnh viewBox này cho phù hợp với SVG của bạn
+      width={width}
+      height={height}
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
-        {/* Dán nội dung SVG của bạn vào đây */}
+      {/* Bạn có thể dán mã SVG của mình vào đây */}
     </svg>
-);
+  );
+}
