72e5c4dd365c4c3bc6cf11c8f35da511ab2f3fdd
Chu Ngọc Sơn <chungocson12a6@gmail.com>
2025-09-18 02:35:54 +0000

    ở /dashboard, các thành phần bị dính vào nhau nên hãy căn chỉnh lại

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -70,7 +70,7 @@ export default function DashboardPage() {
         ))}
       </div>
 
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+      <div className="mt-4 grid gap-4 md:grid-cols-2 lg:grid-cols-7">
         <Card className="col-span-4">
           <CardHeader>
             <CardTitle>Continue Learning</CardTitle>
