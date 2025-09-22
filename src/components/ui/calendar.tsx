e83b85861ecc7e639164f0f6d293ea148dbe8163
Chu Ngọc Sơn <chungocson12a6@gmail.com>
2025-09-22 05:00:07 +0000

    cái cột chọn năm đang bị qua dài

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -25,7 +25,7 @@ function Calendar({
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium hidden",
-        caption_dropdowns: "flex justify-center gap-1",
+        caption_dropdowns: "flex justify-center gap-1 [&_.rdp-vhidden]:w-16",
         vhidden: "",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
