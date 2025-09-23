5a02bad2da84a7ea52680f01d9c50e8797ccc962
Chu Ngọc Sơn <chungocson12a6@gmail.com>
2025-09-23 02:57:10 +0000

    tôi chọn ngày 2 tháng 1 năm 2004 và khi nhấn lên lại thì nó hiện ngày hi

--- src/app/(app)/profile/page.tsx
+++ src/app/(app)/profile/page.tsx
@@ -154,6 +154,7 @@ export default function ProfilePage() {
                                 mode="single"
                                 selected={field.value}
                                 onSelect={field.onChange}
+                                month={field.value}
                                 disabled={(date) =>
                                   date > new Date() || date < new Date("1900-01-01")
                                 }
